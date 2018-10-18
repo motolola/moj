96777f48d5a45c63cfe5047d91888a9c99dfcfc5
Motolola <motolola@icloud.com>
2018-10-18 14:46:41 +0100

    First Commit

---
+++ src/main/java/com/motolola/moj/MojApplication.java
@@ -0,0 +1,12 @@
+package com.motolola.moj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojApplication.class, args);
+	}
+}
